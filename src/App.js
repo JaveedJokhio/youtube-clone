18c1baa3f68bd4b80109774cace083be0d4ddcac
Javed <javeedjokhio48@gmail.com>
2023-08-02 12:15:10 +0500

    "Second Commit"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+// Import components from './components' folder
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Youtube clone app</h1>
-    </div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
